285d9bcc9afa97db3f2091778acaddb665c17c14
tradash <bobkovae@gmail.com>
2018-10-15 15:06:29 +0300

    second commit

--- runme.sh
+++ runme.sh
@@ -3,18 +3,18 @@ docker stop $(docker ps -f "name=tradash-")
 docker rm $(docker ps -f "name=tradash-")
 docker rmi -f $(docker images "*tradash-*" -q)
 
-#if [ -d ./gp ]; then 
-#rm -rf ./gp
-#fi
-#if [ -d ./gpw ]; then 
-#rm -rf ./gpw
-#fi
+if [ -d ./gp ]; then 
+rm -rf ./gp
+fi
+if [ -d ./gpw ]; then 
+rm -rf ./gpw
+fi
 
-#git clone https://github.com/tradash/gp
-#git clone https://github.com/tradash/gpw
+git clone https://github.com/tradash/gp
+git clone https://github.com/tradash/gpw
 
-#sed -i 's/localhost:27017/tradash-mongo:27017/g' ./gp/dbprovider.js
-#sed -i 's/localhost:27017/tradash-mongo:27017/g' ./gpw/dbprovider.js
+sed -i 's/localhost:27017/tradash-mongo:27017/g' ./gp/dbprovider.js
+sed -i 's/localhost:27017/tradash-mongo:27017/g' ./gpw/dbprovider.js
 
 cp gp-dockerfile ./gp/dockerfile
 cp gpw-dockerfile ./gpw/dockerfile
